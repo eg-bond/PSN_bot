ab025974edad7888426263a0815195b9ceaa0f25
eg-bond <eg-bond@yandex.ru>
2023-11-24 12:38:47 +0300

    ps plus block part 1

--- index.js
+++ index.js
@@ -47,6 +47,29 @@ const mainMenuOptions = {
   }),
 }
 
+const psPlusOptions = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        { text: 'PS+ Essential', callback_data: '/ps_plus_ess' },
+        { text: 'PS+ Extra', callback_data: '/ps_plus_ext' },
+        { text: 'PS+ Deluxe', callback_data: '/ps_plus_del' },
+      ],
+      [{ text: 'Подробнее о подписках', callback_data: '/about_ps_plus' }],
+    ],
+  }),
+}
+
+const psPlusEssOptions = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: 'PS+ Essential 1 месяц', callback_data: '/ps_plus_ess_1m' }],
+      [{ text: 'PS+ Essential 3 месяца', callback_data: '/ps_plus_ess_3m' }],
+      [{ text: 'PS+ Essential 12 месяцев', callback_data: '/ps_plus_ess_12m' }],
+    ],
+  }),
+}
+
 const start = () => {
   // makes set of predefined commands, accessible in tg menu
   bot.setMyCommands([
@@ -103,20 +126,59 @@ const start = () => {
   })
 
   bot.on('callback_query', async msg => {
-    console.log(msg)
+    // console.log(msg)
     const data = msg.data
     const chatId = msg.message.chat.id
 
     if (data === '/ps_plus') {
-      await bot.sendMessage(chatId, 'Какую подписку хотите приобрести?')
+      return await bot.sendMessage(
+        chatId,
+        'Какую подписку хотите приобрести?',
+        psPlusOptions
+      )
+    }
+    // PS plus block -------------------------------------
+    if (data === '/about_ps_plus') {
+      return await bot.sendMessage(
+        chatId,
+        ` PS Plus Essential
+        PS Plus Essential представляет собой самый доступный вариант. В его рамках предоставляются 3 бесплатные игры каждый месяц, доступ к онлайн-играм, дополнительные скидки, облачное хранилище, game help и эксклюзивный контент.
+        
+        🔹 PS Plus Extra
+        PS Plus Extra представляет собой улучшенную версию PS Plus. В дополнение к возможностям Essential, она дает доступ к библиотеке из 450+ бесплатных игр, которая пополняется новыми каждый месяц.
+        
+        🔹 PS Plus Deluxe
+        PS Plus Deluxe – это самый расширенный вариант PS Plus, включающий более 900 игр. В эту подписку входит все, что предлагают предыдущие версии, а также каталог классических игр от Sony для PS1 и PS2 (более 100 игр) и пробные версии самых свежих релизов.`,
+        psPlusOptions
+      )
     }
 
-    // return await bot.sendMessage(
-    //   chatId,
-    //   `Petux, была загадана цифра ${bd[chatId]}`,
-    //   againOptions
-    // )
-    //
+    if (data === '/ps_plus_ess') {
+      return await bot.sendMessage(
+        chatId,
+        'Подписки и игры приобретаются через аккаунт поддержки (Тут должен быть аккаунт поддержки lul)',
+        psPlusEssOptions
+      )
+    }
+
+    if (
+      data === '/ps_plus_ess_1m' ||
+      data === '/ps_plus_ess_3m' ||
+      data === '/ps_plus_ess_12m'
+    ) {
+      await bot.sendMessage(chatId, 'Перевожу на аккаунт поддержки')
+      setTimeout(async () => {
+        await bot.sendMessage(
+          chatId,
+          'Ну, на самом деле проcто делаю вид что перевожу'
+        )
+      }, 1000)
+      setTimeout(async () => {
+        await bot.sendMessage(chatId, 'Аккаунта то нет')
+      }, 2000)
+    }
+
+    // /ps_plus_ess_1m
   })
 }
 
